b6e8e5fcbb2ecbcad6751c7073626d5394b60dd3
Ian Kevan <ian.s.kevan@gmail.com>
2022-02-04 10:55:24 +0000

    Updated to latest library

--- app/src/main/java/com/isk/iskdemo/Application/ISKApplication.java
+++ app/src/main/java/com/isk/iskdemo/Application/ISKApplication.java
@@ -20,14 +20,14 @@ public class ISKApplication extends Application {
 	public void onCreate() {
 		super.onCreate();
 
-		List<String> sslPins = new ArrayList<String>();
-		sslPins.add("sha256/s/pS3RZ80zGSt0LxpzvvE462hDL3apULVVBtQRbLYnQ=");
-		sslPins.add("sha256/JSMzqOOrtyOT1kmau6zKhgT676hGgczD5VMdRMyJZFA=");
-		sslPins.add("sha256/++MBgDH5WGvL9Bcn5Be30cRcL0f5O+NyoXuWtQdX1aI=");
-		sslPins.add("sha256/KwccWaCgrnaw6tsrrSO61FgLacNgG2MMLq8GE6+oP5I=");
-		SmartCallingManager.init(this, "https://portal-uat.smartcom.net/", sslPins, null);
+//		List<String> sslPins = new ArrayList<String>();
+//		sslPins.add("sha256/s/pS3RZ80zGSt0LxpzvvE462hDL3apULVVBtQRbLYnQ=");
+//		sslPins.add("sha256/JSMzqOOrtyOT1kmau6zKhgT676hGgczD5VMdRMyJZFA=");
+//		sslPins.add("sha256/++MBgDH5WGvL9Bcn5Be30cRcL0f5O+NyoXuWtQdX1aI=");
+//		sslPins.add("sha256/KwccWaCgrnaw6tsrrSO61FgLacNgG2MMLq8GE6+oP5I=");
+//		SmartCallingManager.init(this, "https://portal-uat.smartcom.net/", sslPins, null);
 
-//		SmartCallingManager.init(this, "https://3431c210eae7.ngrok.io/", null, null);
+		SmartCallingManager.init(this, "https://4b1e-151-230-4-4.ngrok.io/", null, null);
 
 		Globals.smartCallingManager = SmartCallingManager.getInstance();
 
